f71181edec6963d2f4598b74e9171b46fa74183c
Jack Winslow <jackwinslow35@gmail.com>
2022-11-04 12:34:37 -0400

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Jack Winslow on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
